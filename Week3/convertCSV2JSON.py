de115473a44458cce3fad64cfda5bd96dc035c76
Siebren Kazemier <Siebren_kazemier@hotmail.com>
2019-05-01 23:30:19 +0200

    Opdracht week3 (nog niet compleet)

---
+++ Week3/convertCSV2JSON.py
@@ -0,0 +1,27 @@
+###############################################################################
+# Name: Siebren Kazemier
+# School: Uva
+# Project: Assignment week 3, Converting CSV file to JSON
+###############################################################################
+
+import pandas as pd
+
+
+def main(input_file, output_file):
+
+    csv_file = read_csv(input_file)
+    json_file = write_json(csv_file, output_file)
+
+
+def read_csv(input_file):
+    csv_file = pd.read_csv(input_file)
+    return csv_file
+
+
+def write_json(csv_file, output_file):
+    json_file = csv_file.to_json(path_or_buf=output_file)
+    return json_file
+
+
+if __name__ == "__main__":
+    main("temperatuur.csv", "data.json")
